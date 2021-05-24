0681b6b99d8feb19b3ad378772a911d851da30c4
hevarrr <84751215+hevarrr@users.noreply.github.com>
2021-05-25 02:51:57 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ2NTI4NzkyMDk5ODE1NDI0.YKw1Xw.02O8rdAFfVR33ba-QrzniPCX3mc',
+        prefix: '+',
+        activity: 'ACTIVITY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
